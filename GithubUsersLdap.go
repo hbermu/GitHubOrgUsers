d76f59d4488183127e54d34b63f5bb4e748d54a6
hbermu <hbermu@protonmail.ch>
2019-10-03 22:32:52 +0200

    Added SQLite feature

--- GithubUsersLdap.go
+++ GithubUsersLdap.go
@@ -20,7 +20,9 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/prometheus/common/log"
 	"github.com/prometheus/common/version"
 	"github.com/spf13/viper"
@@ -29,6 +31,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/smtp"
+	"os"
 	"path"
 	"strconv"
 	"strings"
@@ -61,6 +64,11 @@ const (
 	DerefInSearching    	= 1
 	DerefFindingBaseObj 	= 2
 	DerefAlways         	= 3
+
+	// SQLite Default Config
+	defaultSQLitePath       = "./sqlite.db"
+	defaultSQLiteUser       = ""
+	defaultSQLitePassword   = ""
 
 	// GitHub Default Config
 	defaultGitHubToken 		= ""
@@ -99,6 +107,10 @@ type Config struct {
 	SMTPFromName	string
 	SMTPToAddresses	[]string
 
+	SQLitePath,
+	SQLiteUser,
+	SQLitePassword  string
+
 	// GitHub Config
 	GitHubToken,
 	GitHubOrg,
@@ -130,6 +142,12 @@ func readConfig(configPath string) Config{
 	viper.SetDefault("ldap.port", 			defaultLdapPort)
 	viper.SetDefault("ldap.base_dn", 		defaultLdapBaseDN)
 	viper.SetDefault("ldap.search_attribute", defaultLdapSearchAttribute)
+
+	//		SQLite
+	viper.SetDefault("sqlite.path", 		defaultSQLitePath)
+	viper.SetDefault("sqlite.user", 		defaultSQLiteUser)
+	viper.SetDefault("sqlite.password", 		defaultSQLitePassword)
+
 
 	// 		GitHub
 	viper.SetDefault("github.token",		defaultGitHubToken)
@@ -183,6 +201,9 @@ func readConfig(configPath string) Config{
 		GitHubSuf:     	 viper.GetString	("github.suf"),
 		GitHubIgnore:    strings.Split(viper.GetString	("github.ignore"), ","),
 		GitHubDelete:    viper.GetBool		("github.delete"),
+		SQLitePath:      viper.GetString	("sqlite.path"),
+		SQLiteUser:      viper.GetString	("sqlite.user"),
+		SQLitePassword:  viper.GetString	("sqlite.password"),
 		LdapEnabled:     viper.GetBool		("ldap.enabled"),
 		LdapUser:        viper.GetString	("ldap.user"),
 		LdapPassword:    viper.GetString	("ldap.password"),
@@ -200,9 +221,7 @@ func getUsersLdap(config Config)  []string{
 	log.Debugln("Connect to LDAP server: " + config.LdapHostname + ":" + strconv.Itoa(config.LdapPort) )
 	// Bind with a read only user
 	l, err := ldap.Dial("tcp", fmt.Sprintf("%s:%d", config.LdapHostname, config.LdapPort))
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkError(err)
 	defer l.Close()
 
 	log.Debugln("Using ldap user: ", config.LdapUser)
@@ -210,9 +229,7 @@ func getUsersLdap(config Config)  []string{
 		log.Warnln("Using default password")
 	}
 	err = l.Bind(config.LdapUser, config.LdapPassword)
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkError(err)
 
 	log.Debugln("Creating ldap request")
 	log.Debugln("Searching in Base DN: ", config.LdapBaseDN)
@@ -228,9 +245,7 @@ func getUsersLdap(config Config)  []string{
 	// Get results
 	log.Infoln("Start the ldap search")
 	sr, err := l.Search(searchRequest)
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkError(err)
 
 	log.Debugln("Copy all results to manage them")
 	ldapUsers := make([]string, 0)
@@ -241,6 +256,52 @@ func getUsersLdap(config Config)  []string{
 
 	log.Infoln("Ldap Search Successfully")
 	return ldapUsers
+}
+
+func getUsersSQLite(config Config) []string{
+	log.Info("Getting users form SQLite")
+
+	sqliteUsers := make([]string, 0)
+
+	log.Debugln("Starting SQLite connection")
+	db, err := sql.Open("sqlite3", config.SQLitePath)
+	checkError(err)
+
+	log.Debugln("Getting all rows")
+	rows, err := db.Query("SELECT username FROM github_users")
+	checkError(err)
+	var username string
+
+	for rows.Next() {
+		err = rows.Scan(&username)
+		checkError(err)
+		sqliteUsers = append(sqliteUsers, username)
+		log.Debugln("User getted:", username)
+	}
+
+	return sqliteUsers
+}
+
+func createSQLSchema(path string) {
+	log.Infoln("Creating Database with scheme")
+
+	if path == defaultSQLitePath {
+		log.Warnln("Using default SQLite path")
+	}
+
+	log.Debugln("Starting SQLite connection")
+	db, err := sql.Open("sqlite3", path)
+	checkError(err)
+	defer db.Close()
+
+	sqlStmt := `
+	create table github_users (username text not null primary key, mail text not null, comment text);
+	`
+
+	log.Infoln("SQLite created on", path)
+	_, err = db.Exec(sqlStmt)
+	checkError(err)
+
 }
 
 func getUsersGitHub(config Config) []string{
@@ -277,9 +338,7 @@ func gitHubRequest(config Config, page int) (string, bool){
 	log.Debugln("Attack to url:", apiUrl)
 	log.Debugln("Prepare the request")
 	req, err := http.NewRequest(http.MethodGet, apiUrl, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkError(err)
 
 	if config.GitHubToken != defaultGitHubToken {
 		req.Header.Add("Authorization", "token "+config.GitHubToken)
@@ -292,15 +351,11 @@ func gitHubRequest(config Config, page int) (string, bool){
 	client := http.DefaultClient
 	log.Debugln("Do the request")
 	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkError(err)
 
 	log.Debugln("Read the response body")
 	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkError(err)
 
 	log.Debugln("Close the response body")
 	defer resp.Body.Close()
@@ -418,9 +473,7 @@ func deleteUsersGitHub(config Config, users []string) {
 		log.Debugln("Attack to url:", apiUrl)
 		log.Debugln("Prepare the request")
 		req, err := http.NewRequest(http.MethodDelete, apiUrl, nil)
-		if err != nil {
-			log.Fatal(err)
-		}
+		checkError(err)
 
 		if config.GitHubToken != defaultGitHubToken {
 			req.Header.Add("Authorization", "token "+config.GitHubToken)
@@ -433,9 +486,7 @@ func deleteUsersGitHub(config Config, users []string) {
 		client := http.DefaultClient
 		log.Debugln("Do the request")
 		_, err = client.Do(req)
-		if err != nil {
-			log.Fatal(err)
-		}
+		checkError(err)
 
 		message = message + "\t" + user + "\n"
 	}
@@ -449,6 +500,11 @@ func deleteUsersGitHub(config Config, users []string) {
 	}
 }
 
+func checkError(err error){
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	version.Revision 	= Revision
@@ -458,60 +514,75 @@ func main() {
 	version.Version		= Version
 
 	var (
-		configPath		= kingpin.Flag("config", "Config file path").Short('c').Default(defaultConfigPath).String()
+
+		createSQLite	= kingpin.Command("create_db", "Create new SQLite Database")
+		createSQLitePath = createSQLite.Arg("sqlite path", "Path to create the SQLite DB").Default(defaultSQLitePath).String()
+
+		start			= kingpin.Command("start", "Start to compare Users with GitHub")
+		configPath		= start.Arg("config path", "Config file path").Default(defaultConfigPath).String()
+
 	)
 
 	// Check flags
 	log.AddFlags(kingpin.CommandLine)
 	kingpin.Version(version.Print(program))
 	kingpin.HelpFlag.Short('h')
-	kingpin.Parse()
 
 	// Log Start
 	log.Infoln("Starting " + program + " ", version.Info())
 	log.Infoln("Build context", version.BuildContext())
 
-	config := readConfig(*configPath)
+	switch kingpin.Parse() {
+	case "create_db":
 
-	var companyUsers []string
-	if config.LdapEnabled {
-		companyUsers = getUsersLdap(config)
+		createSQLSchema(*createSQLitePath)
+
+	case "start":
+		config := readConfig(*configPath)
+
+		var companyUsers []string
+		if config.LdapEnabled {
+			companyUsers = getUsersLdap(config)
+		}
+
+		usersGitHub := getUsersGitHub(config)
+
+		wrongUsersNoSuf, rightUsers := checkExistSuf(config, usersGitHub)
+		wrongUsersNoSufNoRecon, wrongUsersNoSufRecon := compareUsersLists(config, companyUsers, wrongUsersNoSuf)
+
+		wrongUsers,_ := compareUsersLists(config, companyUsers, rightUsers)
+
+		subject := "Subject: Usuarios erroneos en GitHub\n\n"
+		message := "Hola:\n\nLos usuarios en la organización de" + config.GitHubOrg + "GitHub de las siguientes listas no" +
+			"cumplen con las directrices y deberían ser eliminados inmediatamente:\n" +
+			"Usuarios reconocidos sin el sufijo:\n"
+		for _,user := range wrongUsersNoSufRecon {
+			message = message + "\t" + user + "\n"
+		}
+		message = message + "Usuarios con el sufijo no reconocidos:\n"
+		for _,user := range wrongUsers {
+			message = message + "\t" + user + "\n"
+		}
+		message = message + "Usuarios no reconocidos:\n"
+		for _,user := range wrongUsersNoSufNoRecon {
+			message = message + "\t" + user + "\n"
+		}
+
+		if config.SMTPEnabled {
+			sendMailReport(config, message, subject)
+		} else {
+			log.Warnln("SMTP disabled")
+			log.Warnln("The mail will not send")
+			log.Warnln(message)
+		}
+
+		if config.GitHubDelete {
+			deleteUsersGitHub(config, wrongUsers)
+		} else {
+			log.Warnln("Delete GitHub users disabled")
+		}
+
 	}
 
-	usersGitHub := getUsersGitHub(config)
-
-	wrongUsersNoSuf, rightUsers := checkExistSuf(config, usersGitHub)
-	wrongUsersNoSufNoRecon, wrongUsersNoSufRecon := compareUsersLists(config, companyUsers, wrongUsersNoSuf)
-
-	wrongUsers,_ := compareUsersLists(config, companyUsers, rightUsers)
-
-	subject := "Subject: Usuarios erroneos en GitHub\n\n"
-	message := "Hola:\n\nLos usuarios en la organización de" + config.GitHubOrg + "GitHub de las siguientes listas no" +
-		"cumplen con las directrices y deberían ser eliminados inmediatamente:\n" +
-		"Usuarios reconocidos sin el sufijo:\n"
-	for _,user := range wrongUsersNoSufRecon {
-		message = message + "\t" + user + "\n"
-	}
-	message = message + "Usuarios con el sufijo no reconocidos:\n"
-	for _,user := range wrongUsers {
-		message = message + "\t" + user + "\n"
-	}
-	message = message + "Usuarios no reconocidos:\n"
-	for _,user := range wrongUsersNoSufNoRecon {
-		message = message + "\t" + user + "\n"
-	}
-
-	if config.SMTPEnabled {
-		sendMailReport(config, message, subject)
-	} else {
-		log.Warnln("SMTP disabled")
-		log.Warnln("The mail will not send")
-		log.Warnln(message)
-	}
-
-	if config.GitHubDelete {
-		deleteUsersGitHub(config, wrongUsers)
-	} else {
-		log.Warnln("Delete GitHub users disabled")
-	}
+	os.Exit(0)
 }
